ee856971c384936085b05910de29fb2d05308ca7
dadjierteh <dantetteh96@gmail.com>
2015-11-12 22:57:14 +0000

    clear

--- course.php
+++ course.php
@@ -5,6 +5,24 @@
 		
 		function courses(){
 		
-		}    
+		}  
+
+		/** 
+		*This is the view course code that queries the cdatabase and 
+		*allows users to view a course.
+	 	*@param $code This specifies the course by its course code
+	 	*@param $semester This specifies the course by its semester (Spring, Summer or Year)
+	 	*@param $year This specifies the course by its academic year (2015, 2016, ect) 
+	 	*@return Output a view page that allow the user to view the course
+	 	**/
+		function get_course($code, $semester, $year){ 
+			$str_query="Select * from se_course where course_code=$code AND
+					semester=$semester AND year=$year";
+			if(!$this->query($str_query)){
+					return false;
+				}
+                    return $this->fetch();
+			
+			}  
      }
 ?>
