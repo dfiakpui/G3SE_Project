356bf7314d5cc6f559f964deb413c58803abfa7e
kobina-nsiah <obed.kobina.nsiah@ashesi.edu.gh>
2015-11-23 00:18:48 +0000

    documented the function

--- classes/book.php
+++ classes/book.php
@@ -8,6 +8,15 @@ class book extends adb{
 		
 	}
 	
+	/**
+	* This adds a book to the database
+	* @param $code the barcode of the book
+	* @param $name the name of the book
+	* @param $author the author of the book
+	* @param $course_code the code of the course that uses this book
+	* @param $year the year the book was used
+	* @param $semester the semester the book was used
+	*/
 	function add_book($code, $name, $author, $course_code, $year, $semester){
 		$str_query = "insert into se_books set book_code = '$code', book_name = '$name', author = '$author',
 						course_code = '$course_code', year = '$year', semester = '$semester'";
