b8b21f74fe148a522443d16610e99e971e2fb7a3
kobina-nsiah <obed.kobina.nsiah@ashesi.edu.gh>
2015-11-23 00:15:07 +0000

    book constructor written and add_book function written

--- classes/book.php
+++ classes/book.php
@@ -2,6 +2,17 @@
 include_once("adb.php");
 
 class book extends adb{
+
+	//book constructor
+	function book(){
+		
+	}
 	
+	function add_book($code, $name, $author, $course_code, $year, $semester){
+		$str_query = "insert into se_books set book_code = '$code', book_name = '$name', author = '$author',
+						course_code = '$course_code', year = '$year', semester = '$semester'";
+			
+		return $this->query($str_query);		
+	}
 }
 ?>
