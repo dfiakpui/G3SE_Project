d10702898dabb645c1b2de4eebcf075ff3ad5eac
kobina-nsiah <obed.kobina.nsiah@ashesi.edu.gh>
2015-12-03 23:14:22 +0000

    wrote a get_book_by_course function

--- classes/book.php
+++ classes/book.php
@@ -23,5 +23,24 @@ class book extends adb{
 			
 		return $this->query($str_query);		
 	}
+	
+	/**
+	* This adds a book to the database
+	* @param $code the barcode of the book
+	* @param $name the name of the book
+	* @param $author the author of the book
+	* @param $course_code the code of the course that uses this book
+	* @param $year the year the book was used
+	* @param $semester the semester the book was used
+	*/
+	function get_book($code, $name, $author, $other, $course_code, $year, $semester){
+		$str_query="select * from se_book where course_code=$code AND semester=$semester AND year=$year";
+				
+			if(!$this->query($str_query)){
+				return false;
+			}
+				
+			return $this->fetch();		
+	}
 }
 ?>
